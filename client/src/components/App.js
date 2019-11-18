7421b13d4594857af58b6551ffd522564a7690aa
cynthiaChanChan <mession01@gmail.com>
2019-11-18 16:19:43 +0800

    Component Scaffolding and wiring up routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-    return <div>App</div>;
+    return <BrowserRouter>
+        <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/stream/new" exact component={StreamCreate} />
+            <Route path="/stream/edit" exact component={StreamEdit} />
+            <Route path="/stream/delete" exact component={StreamDelete} />
+            <Route path="/stream/show" exact component={StreamShow} />
+        </div>
+    </BrowserRouter>
 };
 
 export default App;
