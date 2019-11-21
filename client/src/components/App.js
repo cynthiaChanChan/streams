89ea1b2902e9ab58550c00190ae83cce482a6cc8
cynthiaChanChan <mession01@gmail.com>
2019-11-21 20:45:10 +0800

    Handling Forms with Redux Form'

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
         <div className="ui container">
             <BrowserRouter>
                 <div>
-                    <Header/>
+                    {/* <Header/> */}
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
                     <Route path="/streams/edit" exact component={StreamEdit} />
